11decd75d374da66530287333843fe63ffa13c36
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-01-18 19:53:52 +0000

    adding build script, environment variables + versioning

--- main.go
+++ main.go
@@ -8,8 +8,30 @@ import (
 )
 
 func main() {
-	c := cli.NewCLI("app", "1.0.0")
+	c := cli.NewCLI("app", Version)
 	c.Args = os.Args[1:]
+
+	// Grab the email from Environment variable
+	if os.Getenv("UKC_EMAIL") != "" {
+		c.Args = append(c.Args, "-email="+os.Getenv("UKC_EMAIL"))
+	}
+
+	// Grab the password from Environment variable
+	if os.Getenv("UKC_PASSWORD") != "" {
+		c.Args = append(c.Args, "-password="+os.Getenv("UKC_PASSWORD"))
+	}
+
+	// Override the default CLI version stuff
+	for _, arg := range c.Args {
+		if arg == "-v" || arg == "-version" || arg == "--version" {
+			newArgs := make([]string, len(c.Args)+1)
+			newArgs[0] = "version"
+			copy(newArgs[1:], c.Args)
+			c.Args = newArgs
+			break
+		}
+	}
+
 	c.Commands = Commands
 
 	exitStatus, err := c.Run()
